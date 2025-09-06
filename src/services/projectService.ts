fdb90170610a6a1c64b0b6a8bc118df2aec75af1
Suyash Pradhan <161103166+Suyash060825@users.noreply.github.com>
2025-09-06 13:46:18 +0530

    Create projectService.ts

---
+++ src/services/projectService.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:4000/api/projects";
+
+export const getProjects = async () => {
+  const res = await axios.get(API_URL);
+  return res.data;
+};
+
+export const createProject = async (project: { name: string; status: string }) => {
+  const res = await axios.post(API_URL, project);
+  return res.data;
+};
