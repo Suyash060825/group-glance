dd3f6de6037299c27ae56a74b9ea3bd8a9a77318
Suyash Pradhan <161103166+Suyash060825@users.noreply.github.com>
2025-09-06 14:49:00 +0530

    Update projectService.ts

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/projects";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api/projects";
+
 
 export const getProjects = async () => {
   const res = await axios.get(API_URL);
